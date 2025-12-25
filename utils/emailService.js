c3fc0990b2320079b9ed3cfaedf573bee1078539
SandalaNithin <sandalanithinkumar123@gmail.com>
2025-12-25 14:39:19 +0530

    mongos

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,6 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465, // SSL port
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -14,7 +17,7 @@ const sendEmail = async (options) => {
         to: process.env.EMAIL_USER,
         replyTo: options.email,
         subject: options.subject,
-        html: options.html,  
+        html: options.html,
     };
 
     await transporter.sendMail(message);
